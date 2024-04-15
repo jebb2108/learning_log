0a452da04f7d2862e5f522fb216ee6d3cab391ba
jebb king <gabouchard2002@gmail.com>
2024-04-15 19:20:00 +0300

    secure users info & fix existing prblms

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Topic(models.Model):
     """ Тема, которую изучает пользователь. """
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """ Возвращает строковое предстваление модели. """
         return self.text
+
 
 class Entry(models.Model):
     """ Информация, изученная пользователем по теме. """
